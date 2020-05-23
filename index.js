6d31125ba5975ae49d1474eaa2b6197b29cbb455
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-23 06:44:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrap(gifts){
+  for (let i = 0; i < gifts.length; i++) {
+    
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+
+
+function writeCards(names, event){
+    let thank_you = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thank_you.push(message)
+}
+return thank_you
+}
+
+function countDown(int) {
+    let x = int
+    while (x >= 0) {
+        console.log(x)
+        x--
+
+}
+    
+}
+
